0c4f61b57a1ea0787cd3dd0fc7415598dbb342fb
mizadri <adrigarvk8@gmail.com>
2024-06-23 23:40:13 -0700

    Implementing sliding window

--- array/longest_subarray.py
+++ array/longest_subarray.py
@@ -1,11 +1,19 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        # generate subarrays, check if all fullfil the condition
-        longest_sub = 0
-        for i,xi in enumerate(nums):
-            subarray = [xi]
-            for j in range(i+1,len(nums)): #if abs(xi-nums[j]) :
-                if all( abs(xxi-nums[j]) <= limit for xxi in subarray):
-                    subarray.append(nums[j])
-                else:
-                    break
+        maxValue=0
+        right=0
+        minValue=0
+        left=0
+        longestWindow = 0
+        while(left <= len(nums) and right <= len(nums)):
+            
+            while abs(maxValue - minValue) <= limit and right <= len(nums):
+                maxValue = max(maxValue, nums[right])
+                right += 1
+                longestWindow = max(longestWindow, right-left)
+            
+            while abs(maxValue - minValue) > limit and left <= len(nums):
+                minValue = min(minValue, nums[left])
+                left += 1
+
+        return longestWindow
