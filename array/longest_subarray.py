a5466bc718e944d9e07c998c932fdb0438a53629
mizadri <adrigarvk8@gmail.com>
2024-06-24 22:43:55 -0700

    Fix some stuff

--- array/longest_subarray.py
+++ array/longest_subarray.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         maxValue=0
@@ -5,15 +7,21 @@ class Solution:
         minValue=0
         left=0
         longestWindow = 0
-        while(left <= len(nums) and right <= len(nums)):
+        while(left < len(nums) and right < len(nums)):
             
-            while abs(maxValue - minValue) <= limit and right <= len(nums):
+            while abs(maxValue - minValue) <= limit and right < len(nums):
                 maxValue = max(maxValue, nums[right])
                 right += 1
                 longestWindow = max(longestWindow, right-left)
+
+            maxValue = 0
             
-            while abs(maxValue - minValue) > limit and left <= len(nums):
+            while abs(maxValue - minValue) > limit and left < len(nums):
                 minValue = min(minValue, nums[left])
                 left += 1
 
         return longestWindow
+    
+sol = Solution()
+res = sol.longestSubarray([8,2,4,7], 4)
+print(res) # 2
