9598064a2ae1ff770b136e27a386bb789e017f94
mizadri <adrigarvk8@gmail.com>
2024-06-25 23:11:29 -0700

    Solved with window but time exceeded

--- array/longest_subarray.py
+++ array/longest_subarray.py
@@ -3,24 +3,24 @@ from typing import List
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         maxValue=0
-        right=0
         minValue=0
+        right=0
         left=0
         longestWindow = 0
         while(left < len(nums) and right < len(nums)):
             
             while abs(maxValue - minValue) <= limit and right < len(nums):
-                maxValue = max(maxValue, nums[right])
-                right += 1
+                maxValue = max(maxValue, nums[right])                
+                minValue = min(nums[left:right+1])
                 longestWindow = max(longestWindow, right-left)
+                right += 1
 
-            maxValue = 0
-            
             while abs(maxValue - minValue) > limit and left < len(nums):
-                minValue = min(minValue, nums[left])
                 left += 1
+                maxValue = max(nums[left:right+1])
+                minValue = min(nums[left:right+1])
 
-        return longestWindow
+        return max(longestWindow, right-left)
     
 sol = Solution()
 res = sol.longestSubarray([8,2,4,7], 4)
