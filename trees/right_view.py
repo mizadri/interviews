5466deb12195f8a6143c9588bb09f42077852795
mizadri <adrigarvk8@gmail.com>
2024-09-05 23:54:52 +0530

    Added right_view queue implementation

---
+++ trees/right_view.py
@@ -0,0 +1,22 @@
+from collections import deque
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        queue = deque([root])
+        right_view = []
+        
+        while queue:
+            level_length = len(queue)
+            for i in range(level_length):
+                node = queue.popleft()
+                if i == level_length - 1:
+                    right_view.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return right_view
