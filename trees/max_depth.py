48f8e4b8c10fda032e415965da5b1bdbdd501cd2
mizadri <adrigarvk8@gmail.com>
2024-09-09 23:55:35 +0530

    Cleanup headers

--- trees/max_depth.py
+++ trees/max_depth.py
@@ -1,9 +1,4 @@
 # Definition for a binary tree node. https://leetcode.com/problems/maximum-depth-of-binary-tree/
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:         
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root:
