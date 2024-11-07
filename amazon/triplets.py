11d6db45506ab3ba58c2cf5292bc2286c42954f4
mizadri <adrigarvk8@gmail.com>
2024-11-07 22:30:01 +0000

    Triplets simplified

--- amazon/triplets.py
+++ amazon/triplets.py
@@ -13,9 +13,37 @@ class Solution:
             for j in range(len(nums)):
                 for k in range(len(nums)):
                     if (i != j and i != k and j != k):
-                        triplet = frozenset([i,j,k])
+                        triplet = frozenset([nums[i],nums[j],nums[k]])
 
                         if sum([nums[ix] for ix in triplet]) == 0:
                             triplets.add(triplet)
 
         return [[nums[ix] for ix in triplet] for triplet in triplets]
+    
+
+
+    """
+    
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        triplets = set()
+        target_elements_ixs = {}
+        results = set()
+        for i in range(len(nums)):
+            fixed_element = nums[i]
+            # Do two sum after fixing i
+            for j in range(len(nums)):
+                if i != j:
+                    target_elements_ixs[-fixed_element-nums[j]] = j
+            for k in range(len(nums)):
+                if i != k:
+                    cur_el = nums[k]
+                    if cur_el in target_elements_ixs:
+                        jj = target_elements_ixs[cur_el]
+                        if jj != k:
+                            triplet = [fixed_element,nums[jj],cur_el]
+                            if sum(triplet) == 0:
+                                results.add(tuple(sorted(triplet)))
+        return [list(triples) for triples in results]
+    
+    """
