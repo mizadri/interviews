baca487403ab7435443598e5a7762d4377ed4093
mizadri <adrigarvk8@gmail.com>
2024-11-16 23:33:34 +0000

    Parenthesis stack implementation

--- easy/parenthesis.py
+++ easy/parenthesis.py
@@ -1,38 +1,17 @@
-"""
-20. Valid Parentheses
-Easy
-Topics
-Companies
-Hint
-Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
-An input string is valid if:
-
-Open brackets must be closed by the same type of brackets.
-Open brackets must be closed in the correct order.
-Every close bracket has a corresponding open bracket of the same type.
- 
-
-Example 1:
-
-Input: s = "()"
-Output: true
-Example 2:
-
-Input: s = "()[]{}"
-Output: true
-Example 3:
-
-Input: s = "(]"
-Output: false
- 
-
-Constraints:
-
-1 <= s.length <= 104
-s consists of parentheses only '()[]{}'.
-"""
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        stack = []
+        brackets = {"(":")", "[":"]", "{":"}"}
+        for bracket in s:
+            if bracket in brackets:
+                stack.append(brackets[bracket])
+            else:
+                if len(stack) > 0 and stack[-1] == bracket:
+                    stack.pop()
+                else:
+                    return False
+                    
+        if len(stack) == 0:
+            return True
+        else:
+            return False
