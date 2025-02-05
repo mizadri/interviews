1f65f193e59290cc15a13bd5d06ef9eb2c9702d0
mizadri <adrigarvk8@gmail.com>
2025-02-06 00:33:20 +0400

    Resolve timeout solution

---
+++ js30/resolve_timeout.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise((resolve)=>{setTimeout(()=>{
+            resolve("Hello world!");
+        },millis)
+    });
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
